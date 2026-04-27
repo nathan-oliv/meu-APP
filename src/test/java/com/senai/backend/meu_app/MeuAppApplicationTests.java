0d69d0937f5a46740e1646a4b6a374fa717fce0e
nathan-oliv <nathansdeoliv@gmail.com>
2026-04-27 16:34:50 -0300

    models: clientes e imoveis

---
+++ src/test/java/com/senai/backend/meu_app/MeuAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.backend.meu_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
